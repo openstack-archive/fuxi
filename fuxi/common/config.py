1ab1d03eb1451c4a2cc3faadd88c9aa36ba02543
zhangni <zhangni@huawei.com>
2016-05-26 15:45:43 +0800

    Fuxi code for providing Cinder volume to Container
    Provide create, rm, delete, list, get, mount and unmount Cinder volume
    for Docker Container
    
    Change-Id: I5f37b4a0781ebc21ef054dbcfa5a822404fc522d

---
+++ fuxi/common/config.py
@@ -0,0 +1,113 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+from oslo_config import cfg
+from oslo_log import log as logging
+
+from fuxi import i18n
+from fuxi.version import version_info
+
+_ = i18n._
+
+default_opts = [
+    cfg.StrOpt('my_ip',
+               help=_('IP address of this machine.')),
+    cfg.IntOpt('fuxi_port',
+               default=7879,
+               help=_('Port for fuxi volume driver server')),
+    cfg.StrOpt('volume_dir',
+               default='/fuxi/data',
+               help=_('At which the docker volume will create.')),
+    cfg.ListOpt('volume_providers',
+                help=_('Volume storage backends that provide volume for '
+                       'docker')),
+    cfg.StrOpt('volume_connector',
+               help=_('Volume connector for attach volume to this server, '
+                      'or detach volume from this server.')),
+    cfg.StrOpt('volume_from',
+               default='fuxi',
+               help=_('Setting label for volume.')),
+    cfg.IntOpt('default_volume_size',
+               default=1,
+               help=_('Default size for volume.')),
+    cfg.StrOpt('fstype',
+               default='ext4',
+               help=_('File system type for volume.')),
+    cfg.BoolOpt('threaded',
+                default=True,
+                help=_('Make this volume plugin run in multi-thread.')),
+    cfg.StrOpt('volume_joiner',
+               default='#',
+               help=_('Connect docker volume name and cinder volume '
+                      'uuid to make up a mountpoint.')),
+    cfg.StrOpt('rootwrap_config',
+               default='/etc/fuxi/rootwrap.conf'),
+]
+keystone_opts = [
+    cfg.StrOpt('region',
+               default=os.environ.get('REGION'),
+               help=_('The region that this machine belongs to.')),
+    cfg.StrOpt('auth_url',
+               default=os.environ.get('IDENTITY_URL'),
+               help=_('The URL for accessing the identity service.')),
+    cfg.StrOpt('admin_user',
+               default=os.environ.get('SERVICE_USER'),
+               help=_('The username to auth with the identity service.')),
+    cfg.StrOpt('admin_tenant_name',
+               default=os.environ.get('SERVICE_TENANT_NAME'),
+               help=_('The tenant name to auth with the identity service.')),
+    cfg.StrOpt('admin_password',
+               default=os.environ.get('SERVICE_PASSWORD'),
+               help=_('The password to auth with the identity service.')),
+    cfg.StrOpt('admin_token',
+               default=os.environ.get('SERVICE_TOKEN'),
+               help=_('The admin token.')),
+    cfg.StrOpt('auth_ca_cert',
+               default=os.environ.get('SERVICE_CA_CERT'),
+               help=_('The CA certification file.')),
+    cfg.BoolOpt('auth_insecure',
+                default=True,
+                help=_("Turn off verification of the certificate for ssl.")),
+]
+
+cinder_opts = [
+    cfg.StrOpt('availability_zone',
+               default=None,
+               help=_('AZ in which the current machine creates, '
+                      'and volume is going to create.')),
+    cfg.StrOpt('volume_type',
+               default=None,
+               help=_('Volume type.')),
+    cfg.StrOpt('protocol',
+               default='iscsi',
+               help=_('This effective when cloud is False.')),
+    cfg.BoolOpt('multiattach',
+                default=False,
+                help=_('Allow the volume to be attached to more than '
+                       'one instance.'))
+]
+
+
+CONF = cfg.CONF
+CONF.register_opts(default_opts)
+CONF.register_opts(keystone_opts, group='keystone')
+CONF.register_opts(cinder_opts, group='cinder')
+
+# Setting oslo.log options for logging.
+logging.register_options(CONF)
+
+
+def init(args, **kwargs):
+    cfg.CONF(args=args, project='fuxi',
+             version=version_info.release_string(), **kwargs)
---
+++ fuxi/controllers.py
@@ -0,0 +1,223 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import collections
+import flask
+import os
+
+from oslo_concurrency import processutils
+from oslo_config import cfg
+from oslo_utils import importutils
+
+from fuxi import app
+from fuxi import exceptions
+from fuxi.i18n import _, _LI, _LW
+from fuxi import utils
+
+CONF = cfg.CONF
+
+CINDER = 'cinder'
+
+volume_providers_conf = {
+    CINDER: 'fuxi.volumeprovider.cinderprovider.Cinder', }
+
+
+def init_app_conf():
+    # Init volume providers.
+    volume_providers = CONF.volume_providers
+    if not volume_providers:
+        raise Exception("Must define volume providers in configuration file.")
+
+    app.volume_providers = collections.OrderedDict()
+    for provider in volume_providers:
+        if provider in volume_providers_conf:
+            app.volume_providers[provider] = importutils\
+                .import_class(volume_providers_conf[provider])()
+            app.logger.info(_LI("Load volume provider: {0}").format(provider))
+        else:
+            msg = _LW("Could not find volume provider: {0}").format(provider)
+            app.logger.warn(msg)
+    if not app.volume_providers:
+        raise Exception("Not provide at least one effective volume provider.")
+
+    # Init volume store directory.
+    try:
+        volume_dir = CONF.volume_dir
+        if not os.path.exists(volume_dir) or not os.path.isdir(volume_dir):
+            utils.execute('mkdir', '-p', '-m=700', volume_dir,
+                          run_as_root=True)
+    except processutils.ProcessExecutionError:
+        raise
+
+
+@app.route('/Plugin.Activate', methods=['POST'])
+def plugin_activate():
+    app.logger.info(_LI("/Plugin.Activate"))
+    return flask.jsonify(Implements=[u'VolumeDriver'])
+
+
+@app.route('/VolumeDriver.Create', methods=['POST'])
+def volumedriver_create():
+    json_data = flask.request.get_json(force=True)
+    app.logger.info(_LI("Received JSON data {0} for "
+                        "/VolumeDriver.Create").format(json_data))
+
+    docker_volume_name = json_data.get('Name', None)
+    volume_opts = json_data.get('Opts', None) or {}
+    if not docker_volume_name:
+        msg = _("Request /VolumeDriver.Create need parameter 'Name'.")
+        app.logger.error(msg)
+        raise exceptions.InvalidInput(msg)
+    if not isinstance(volume_opts, dict):
+        msg = _("Request parameter 'Opts' must be dict type.")
+        app.logger.error(msg)
+        raise exceptions.InvalidInput(msg)
+
+    volume_provider_type = volume_opts.get('volume_provider', None)
+    if not volume_provider_type:
+        volume_provider_type = CINDER
+
+    if volume_provider_type not in app.volume_providers:
+        msg_fmt = _("Could not find a handler for %(volume_provider_type)s "
+                    "volume.") % {'volume_provider_type': volume_provider_type}
+        app.logger.error(msg_fmt)
+        return flask.jsonify(Err=msg_fmt)
+
+    # If the volume with the same name already exists in other volume
+    # provider backend, then raise an error
+    for vpt, provider in app.volume_providers.items():
+        if (volume_provider_type != vpt
+                and provider.check_exist(docker_volume_name)):
+            msg_fmt = _("The volume with the same name already exists in "
+                        "other volume provider backend.")
+            app.logger.error(msg_fmt)
+            return flask.jsonify(Err=msg_fmt)
+
+    app.volume_providers[volume_provider_type].create(docker_volume_name,
+                                                      volume_opts)
+
+    return flask.jsonify(Err=u'')
+
+
+@app.route('/VolumeDriver.Remove', methods=['POST'])
+def volumedriver_remove():
+    json_data = flask.request.get_json(force=True)
+    app.logger.info(_LI("Received JSON data {0} for "
+                        "/VolumeDriver.Remove").format(json_data))
+
+    docker_volume_name = json_data.get('Name', None)
+    if not docker_volume_name:
+        msg = _("Request /VolumeDriver.Remove need parameter 'Name'.")
+        app.logger.error(msg)
+        raise exceptions.InvalidInput(msg)
+
+    for provider in app.volume_providers.values():
+        if provider.delete(docker_volume_name):
+            return flask.jsonify(Err=u'')
+
+    return flask.jsonify(Err=u'')
+
+
+@app.route('/VolumeDriver.Mount', methods=['POST'])
+def volumedriver_mount():
+    json_data = flask.request.get_json(force=True)
+    app.logger.info(_LI("Receive JSON data {0} for "
+                        "/VolumeDriver.Mount").format(json_data))
+
+    docker_volume_name = json_data.get('Name', None)
+    if not docker_volume_name:
+        msg = _("Request /VolumeDriver.Mount need parameter 'Name'.")
+        app.logger.error(msg)
+        raise exceptions.InvalidInput(msg)
+
+    for provider in app.volume_providers.values():
+        if provider.check_exist(docker_volume_name):
+            mountpoint = provider.mount(docker_volume_name)
+            return flask.jsonify(Mountpoint=mountpoint, Err=u'')
+
+    return flask.jsonify(Err=u'Mount Failed')
+
+
+@app.route('/VolumeDriver.Path', methods=['POST'])
+def volumedriver_path():
+    json_data = flask.request.get_json(force=True)
+    app.logger.info(_LI("Receive JSON data {0} for "
+                        "/VolumeDriver.Path").format(json_data))
+
+    docker_volume_name = json_data.get('Name', None)
+    if not docker_volume_name:
+        msg = _("Request /VolumeDriver.Path need parameter 'Name'.")
+        app.logger.error(msg)
+        raise exceptions.InvalidInput(msg)
+
+    for provide in app.volume_providers.values():
+        try:
+            mountpoint = provide.path(docker_volume_name)
+            app.logger.info("Get mountpoint %(mp)s for docker volume "
+                            "%(name)s" % {'mp': mountpoint,
+                                          'name': docker_volume_name})
+            return flask.jsonify(Mountpoint=mountpoint, Err=u'')
+        except exceptions.NotFound:
+            pass
+
+    app.logger.warn(_LW("Can't find mountpoint for docker volume "
+                        "%(name)s") % {'name': docker_volume_name})
+    return flask.jsonify(Err=u'Mountpoint Not Found')
+
+
+@app.route('/VolumeDriver.Unmount', methods=['POST'])
+def volumedriver_unmount():
+    json_data = flask.request.get_json(force=True)
+    app.logger.info(_LI('Receive JSON data {0} for '
+                        'VolumeDriver.Unmount').format(json_data))
+    return flask.jsonify(Err=u'')
+
+
+@app.route('/VolumeDriver.Get', methods=['POST'])
+def volumedriver_get():
+    json_data = flask.request.get_json(force=True)
+    app.logger.info(_LI("Receive JSON data {0} for "
+                        "/VolumeDriver.Get").format(json_data))
+
+    docker_volume_name = json_data.get('Name', None)
+    if not docker_volume_name:
+        msg = _("Request /VolumeDriver.Get need parameter 'Name'.")
+        app.logger.error(msg)
+        raise exceptions.InvalidInput(msg)
+
+    for provider in app.volume_providers.values():
+        try:
+            mountpoint = provider.show(docker_volume_name)
+            volume = {'Name': docker_volume_name, 'Mountpoint': mountpoint}
+            msg_fmt = _LI("Get docker volume: {0}").format(volume)
+            app.logger.info(msg_fmt)
+            return flask.jsonify(Volume=volume, Err=u'')
+        except exceptions.NotFound:
+            pass
+
+    app.logger.warn(_LW("Can't find volume {0} from every "
+                        "provider.").format(docker_volume_name))
+    return flask.jsonify(Err=u'Volume Not Found')
+
+
+@app.route('/VolumeDriver.List', methods=['POST'])
+def volumedriver_list():
+    app.logger.info(_LI("/VolumeDriver.List"))
+    docker_volumes = []
+    for provider in app.volume_providers.values():
+        vs = provider.list()
+        if vs:
+            docker_volumes.extend(vs)
+
+    app.logger.info(_LI("Get volumes from volume providers. "
+                        "Volumes: {0}").format(docker_volumes))
+    return flask.jsonify(Err=u'', Volumes=docker_volumes)
