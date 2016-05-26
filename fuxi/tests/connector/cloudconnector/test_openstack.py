7fdade1c5a1acff69ab97c17aad7e4aef17bc706
zhangni <zhangni@huawei.com>
2016-05-26 15:45:43 +0800

    Fuxi code for providing Cinder volume to Container
    Provide create, rm, delete, list, get, mount and unmount Cinder volume
    for Docker Container
    
    Change-Id: I5f37b4a0781ebc21ef054dbcfa5a822404fc522d

---
+++ fuxi/tests/connector/cloudconnector/test_openstack.py
@@ -0,0 +1,105 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+import os
+
+from fuxi.common import constants
+from fuxi.common import state_monitor
+from fuxi.connector.cloudconnector import openstack
+from fuxi import utils
+from fuxi.tests import base, fake_client, fake_object
+
+from cinderclient import exceptions as cinder_exception
+from novaclient import exceptions as nova_exception
+
+
+def mock_list_with_attach_to_this(cls, search_opts={}):
+    attachments = [{u'server_id': u'123',
+                    u'attachment_id': u'123',
+                    u'attached_at': u'2016-05-20T09:19:57.000000',
+                    u'host_name': None,
+                    u'device': None,
+                    u'id': u'123'}]
+    return [fake_object.FakeCinderVolume(name='fake-vol1',
+                                         attachments=attachments)]
+
+
+def mock_list_with_attach_to_other(cls, search_opts={}):
+    attachments = [{u'server_id': u'1234',
+                    u'attachment_id': u'123',
+                    u'attached_at': u'2016-05-20T09:19:57.000000',
+                    u'host_name': None,
+                    u'device': None,
+                    u'id': u'123'}]
+    return [fake_object.FakeCinderVolume(name='fake-vol1',
+                                         attachments=attachments)]
+
+
+def mock_get_mountpoint_for_device(devpath, mountpoint):
+    return ''
+
+
+class TestCinderConnector(base.TestCase):
+    def setUp(self):
+        base.TestCase.setUp(self)
+        self.connector = openstack.CinderConnector()
+        self.connector.cinderclient = fake_client.FakeCinderClient()
+        self.connector.novaclient = fake_client.FakeNovaClient()
+
+    def test_connect_volume(self):
+        pass
+
+    @mock.patch.object(utils, 'get_instance_uuid', return_value='fake-123')
+    @mock.patch.object(utils, 'execute')
+    @mock.patch.object(state_monitor.StateMonitor, 'monitor_cinder_volume',
+                       return_value=None)
+    def test_disconnect_volume(self, mock_inst_id, mock_execute, mock_monitor):
+        fake_cinder_volume = fake_object.FakeCinderVolume()
+        result = self.connector.disconnect_volume(fake_cinder_volume)
+        self.assertIsNone(result)
+
+    @mock.patch('fuxi.tests.fake_client.FakeCinderClient.Volumes.get',
+                side_effect=cinder_exception.ClientException(404))
+    @mock.patch.object(utils, 'execute')
+    @mock.patch.object(state_monitor.StateMonitor,
+                       'monitor_cinder_volume')
+    def test_disconnect_volume_for_not_found(self, mock_get, mock_execute,
+                                             mocK_monitor):
+        fake_cinder_volume = fake_object.FakeCinderVolume()
+        self.assertRaises(cinder_exception.ClientException,
+                          self.connector.disconnect_volume,
+                          fake_cinder_volume)
+
+    @mock.patch('fuxi.tests.fake_client.FakeNovaClient.Volumes'
+                '.delete_server_volume',
+                side_effect=nova_exception.ClientException(500))
+    @mock.patch.object(utils, 'get_instance_uuid', return_value='fake-123')
+    @mock.patch.object(utils, 'execute')
+    @mock.patch.object(state_monitor.StateMonitor,
+                       'monitor_cinder_volume')
+    def test_disconnect_volume_for_delete_server_volume_failed(self,
+                                                               mock_delete,
+                                                               mock_inst_id,
+                                                               mock_execute,
+                                                               mock_monitor):
+        fake_cinder_volume = fake_object.FakeCinderVolume()
+        self.assertRaises(nova_exception.ClientException,
+                          self.connector.disconnect_volume,
+                          fake_cinder_volume)
+
+    def test_get_device_path(self):
+        fake_cinder_volume = fake_object.FakeCinderVolume()
+        fake_devpath = os.path.join(constants.VOLUME_LINK_DIR,
+                                    fake_cinder_volume.id)
+        self.assertEqual(fake_devpath,
+                         self.connector.get_device_path(fake_cinder_volume))
---
+++ fuxi/utils.py
@@ -0,0 +1,187 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import flask
+import os
+import requests
+import socket
+import traceback
+
+from fuxi.common import constants
+from fuxi import exceptions
+from fuxi.i18n import _LW, _LE
+
+from cinderclient import client as cinder_client
+from cinderclient import exceptions as cinder_exception
+from keystoneauth1.session import Session
+from keystoneclient.auth import get_plugin_class
+from novaclient import client as nova_client
+from novaclient import exceptions as nova_exception
+from os_brick import exception as brick_exception
+from oslo_concurrency import processutils
+from oslo_config import cfg
+from oslo_log import log as logging
+from oslo_utils import importutils
+from oslo_utils import uuidutils
+from werkzeug import exceptions as w_exceptions
+
+cloud_init_conf = '/var/lib/cloud/instances'
+
+CONF = cfg.CONF
+
+LOG = logging.getLogger(__name__)
+
+
+def get_hostname():
+    return socket.gethostname()
+
+
+def get_instance_uuid():
+    try:
+        inst_uuid = ''
+        inst_uuid_count = 0
+        dirs = os.listdir(cloud_init_conf)
+        for uuid_dir in dirs:
+            if uuidutils.is_uuid_like(uuid_dir):
+                inst_uuid = uuid_dir
+                inst_uuid_count += 1
+
+        # If not or not only get on instance_uuid, then search
+        # it from metadata server.
+        if inst_uuid_count == 1:
+            return inst_uuid
+    except Exception:
+        LOG.warning(_LW("Get instance_uuid from cloud-init failed"))
+
+    try:
+        resp = requests.get('http://169.254.169.254/openstack',
+                            timeout=constants.CURL_MD_TIMEOUT)
+        metadata_api_versions = resp.text.split()
+        metadata_api_versions.sort(reverse=True)
+    except Exception as e:
+        LOG.error(_LE("Get metadata apis failed. Error: {}").format(e))
+        raise exceptions.FuxiException("Metadata API Not Found")
+
+    for api_version in metadata_api_versions:
+        metadata_url = ''.join(['http://169.254.169.254/openstack/',
+                                api_version,
+                                '/meta_data.json'])
+        try:
+            resp = requests.get(metadata_url,
+                                timeout=constants.CURL_MD_TIMEOUT)
+            metadata = resp.json()
+            if metadata.get('uuid', None):
+                return metadata['uuid']
+        except Exception as e:
+            LOG.warning(_LW("Get instance_uuid from metadata server {0} "
+                            "failed. Error: {1}").format(metadata_url, e))
+            continue
+
+    raise exceptions.FuxiException("Instance UUID Not Found")
+
+
+# Return all errors as JSON. From http://flask.pocoo.org/snippets/83/
+def make_json_app(import_name, **kwargs):
+    app = flask.Flask(import_name, **kwargs)
+
+    @app.errorhandler(exceptions.FuxiException)
+    @app.errorhandler(cinder_exception.ClientException)
+    @app.errorhandler(nova_exception.ClientException)
+    @app.errorhandler(processutils.ProcessExecutionError)
+    @app.errorhandler(brick_exception.BrickException)
+    def make_json_error(ex):
+        app.logger.error(_LE("Unexpected error happened: %s"),
+                         traceback.format_exc())
+        response = flask.jsonify({"Err": str(ex)})
+        response.status_code = w_exceptions.InternalServerError.code
+        if isinstance(ex, w_exceptions.HTTPException):
+            response.status_code = ex.code
+        content_type = 'application/vnd.docker.plugins.v1+json; charset=utf-8'
+        response.headers['Content-Type'] = content_type
+        return response
+
+    for code in w_exceptions.default_exceptions:
+        app.register_error_handler(code, make_json_error)
+
+    return app
+
+
+def driver_dict_from_config(named_driver_config, *args, **kwargs):
+    driver_registry = dict()
+
+    for driver_str in named_driver_config:
+        driver_type, _sep, driver = driver_str.partition('=')
+        driver_class = importutils.import_class(driver)
+        driver_registry[driver_type] = driver_class(*args, **kwargs)
+    return driver_registry
+
+
+def _openstack_auth_from_config(**config):
+    if config.get('username') and config.get('password'):
+        plugin_class = get_plugin_class('password')
+    else:
+        plugin_class = get_plugin_class('token')
+    plugin_options = plugin_class.get_options()
+    plugin_kwargs = {}
+    for option in plugin_options:
+        if option.dest in config:
+            plugin_kwargs[option.dest] = config[option.dest]
+    return plugin_class(**plugin_kwargs)
+
+
+def get_keystone_session(**kwargs):
+    keystone_conf = CONF.keystone
+    config = {}
+    config['auth_url'] = keystone_conf.auth_url
+    config['username'] = keystone_conf.admin_user
+    config['password'] = keystone_conf.admin_password
+    config['tenant_name'] = keystone_conf.admin_tenant_name
+    config['token'] = keystone_conf.admin_token
+    config.update(kwargs)
+
+    if keystone_conf.auth_insecure:
+        verify = False
+    else:
+        verify = keystone_conf.auth_ca_cert
+
+    return Session(auth=_openstack_auth_from_config(**config), verify=verify)
+
+
+def get_cinderclient(session=None, region=None, **kwargs):
+    if not session:
+        session = get_keystone_session(**kwargs)
+    if not region:
+        region = CONF.keystone['region']
+    return cinder_client.Client(session=session,
+                                region_name=region,
+                                version=2)
+
+
+def get_novaclient(session=None, region=None, **kwargs):
+    if not session:
+        session = get_keystone_session(**kwargs)
+    if not region:
+        region = CONF.keystone['region']
+    return nova_client.Client(session=session,
+                              region_name=region,
+                              version=2)
+
+
+def get_root_helper():
+    return 'sudo fuxi-rootwrap %s' % CONF.rootwrap_config
+
+
+def execute(*cmd, **kwargs):
+    if 'run_as_root' in kwargs and 'root_helper' not in kwargs:
+        kwargs['root_helper'] = get_root_helper()
+
+    return processutils.execute(*cmd, **kwargs)
