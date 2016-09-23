bea742692d2a42e83e2b14599aaf007051add8e7
zhangni <zhangni@huawei.com>
2016-09-23 20:46:13 +0800

    Enable Fuxi to use Manila share
    Support Docker use Manila share as a shared persistent volume.
    For Manila, it supports many driver backends and share_proto,
    in this bp, NFS share_proto will be supported while other
    share_proto are not supported temporarily
    
    Change-Id: Id987c252ad78e37fae07cb1b1ec3e413de962572
    blueprint: enable-fuxi-to-use-manila-share

--- fuxi/utils.py
+++ fuxi/utils.py
@@ -26,6 +26,9 @@ from cinderclient import client as cinder_client
 from cinderclient import exceptions as cinder_exception
 from keystoneauth1.session import Session
 from keystoneclient.auth import get_plugin_class
+from manilaclient import client as manila_client
+from manilaclient.openstack.common.apiclient import exceptions \
+    as manila_exception
 from novaclient import client as nova_client
 from novaclient import exceptions as nova_exception
 from os_brick import exception as brick_exception
@@ -98,6 +101,7 @@ def make_json_app(import_name, **kwargs):
     @app.errorhandler(exceptions.FuxiException)
     @app.errorhandler(cinder_exception.ClientException)
     @app.errorhandler(nova_exception.ClientException)
+    @app.errorhandler(manila_exception.ClientException)
     @app.errorhandler(processutils.ProcessExecutionError)
     @app.errorhandler(brick_exception.BrickException)
     def make_json_error(ex):
@@ -178,6 +182,16 @@ def get_novaclient(session=None, region=None, **kwargs):
                               version=2)
 
 
+def get_manilaclient(session=None, region=None):
+    if not session:
+        session = get_keystone_session()
+    if not region:
+        region = CONF.keystone['region']
+    return manila_client.Client(session=get_keystone_session(),
+                                region_name=region,
+                                client_version='2')
+
+
 def get_root_helper():
     return 'sudo fuxi-rootwrap %s' % CONF.rootwrap_config
 
@@ -191,3 +205,18 @@ def execute(*cmd, **kwargs):
 
 def get_random_string(n=10):
     return ''.join(random.choice(string.ascii_lowercase) for _ in range(n))
+
+
+def wrap_check_authorized(f):
+    """If token is expired, then build a new client, and try again.
+
+    This method required the related object(cls) has method set_client().
+    method set_client() is used to reset OpenStack *client.
+    """
+    def func(cls, *args, **kwargs):
+        try:
+            return f(cls, *args, **kwargs)
+        except manila_exception.Unauthorized:
+            cls.set_client()
+            return f(cls, *args, **kwargs)
+    return func
