483d32dd084018d715f9e3e19dd8b7d71a1c5b53
Hongbin Lu <hongbin.lu@huawei.com>
2016-11-28 23:57:48 +0000

    [WIP] Add basic fullstack volume tests
    Partial-Implements: blueprint fullstack-testing
    Change-Id: I614d8457219d17278aa8d7948b565ad284500e7c

--- fuxi/tests/contrib/gate_hook.sh
+++ fuxi/tests/contrib/gate_hook.sh
@@ -7,4 +7,6 @@ VENV=${1:-"fullstack"}
 GATE_DEST=$BASE/new
 DEVSTACK_PATH=$GATE_DEST/devstack
 
+export DEVSTACK_LOCAL_CONFIG="enable_plugin fuxi https://git.openstack.org/openstack/fuxi"
+
 $BASE/new/devstack-gate/devstack-vm-gate.sh
