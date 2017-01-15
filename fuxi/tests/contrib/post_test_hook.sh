2361477e02d1968b524cdf0fb3edc9e1f94f39b1
Hongbin Lu <hongbin.lu@huawei.com>
2017-01-14 22:48:34 -0600

    Add a config to allow search all tenants
    If users pre-create cinder volumes in their own tenants and pass
    the cinder volume ID to Fuxi, Fuxi will return successfully and
    the docker volume was created. However, Fuxi will fail to search
    the pre-created volumes if users list/inspect the volume. This
    is because Fuxi tried to find volumes in 'service' tenant but
    the volume was pre-created in users tenants. This commit added
    a config to allow search volumes in all tenants.
    
    Related-Bug: #1656960
    Change-Id: Idb4a3611465aa44cf88aa0abe144897849a27b7d

--- fuxi/tests/contrib/post_test_hook.sh
+++ fuxi/tests/contrib/post_test_hook.sh
@@ -44,6 +44,11 @@ owner=stack
 cd $FUXI_DIR
 sudo chown -R $owner:stack $FUXI_DIR
 
+# Get admin credentials
+pushd ../devstack
+source openrc fuxi service
+popd
+
 # Run tests
 echo "Running Fuxi $venv fullstack tests"
 set +e
