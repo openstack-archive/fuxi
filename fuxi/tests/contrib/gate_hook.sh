3372eb6757282c3d1118eb0794af590bf920a4e1
Hongbin Lu <hongbin.lu@huawei.com>
2017-02-26 16:00:24 -0600

    [WIP] Enable manila devstack plugin
    Change-Id: I17a5f1b673a8785d013c74cd855006b5309cd9eb

--- fuxi/tests/contrib/gate_hook.sh
+++ fuxi/tests/contrib/gate_hook.sh
@@ -7,4 +7,7 @@ VENV=${1:-"fullstack"}
 GATE_DEST=$BASE/new
 DEVSTACK_PATH=$GATE_DEST/devstack
 
+export PROJECTS="openstack/python-manilaclient $PROJECTS"
+export DEVSTACK_LOCAL_CONFIG+=$'\n'"enable_plugin manila git://git.openstack.org/openstack/manila"
+
 $BASE/new/devstack-gate/devstack-vm-gate.sh
