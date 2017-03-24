2b455f2b73363ddca0f601a6eb2c099a1c4c120f
Hongbin Lu <hongbin.lu@huawei.com>
2017-03-24 22:36:15 +0000

    [WIP] Add fullstack test for manila provider
    Depends-On: I3adfbf78d0792471a29c7ff979aad1418e6137d4
    Change-Id: I0723e4e000c66b4f2f719f50d11e0e198b870a57

--- fuxi/tests/contrib/gate_hook.sh
+++ fuxi/tests/contrib/gate_hook.sh
@@ -8,5 +8,7 @@ GATE_DEST=$BASE/new
 DEVSTACK_PATH=$GATE_DEST/devstack
 
 export DEVSTACK_LOCAL_CONFIG+=$'\n'"enable_plugin manila git://git.openstack.org/openstack/manila"
+export MANILA_DEFAULT_SHARE_TYPE_EXTRA_SPECS='snapshot_support=True create_share_from_snapshot_support=True revert_to_snapshot_support=True mount_snapshot_support=True'
+export SHARE_DRIVER=manila.share.drivers.lvm.LVMShareDriver
 
 $BASE/new/devstack-gate/devstack-vm-gate.sh
