b8bda4dcf9bf2a78a6f931f34010aa22ea5d9178
Hongbin Lu <hongbin.lu@huawei.com>
2017-04-18 12:30:49 -0400

    [WIP] Make volume providers configurable in devstack
    Change-Id: I29d358ce96e64bb98a7a656975c3b7b1aa523202

--- fuxi/tests/contrib/gate_hook.sh
+++ fuxi/tests/contrib/gate_hook.sh
@@ -11,5 +11,6 @@ export DEVSTACK_LOCAL_CONFIG+=$'\n'"enable_plugin manila git://git.openstack.org
 export DEVSTACK_LOCAL_CONFIG+=$'\n'"MANILA_DEFAULT_SHARE_TYPE_EXTRA_SPECS='snapshot_support=True create_share_from_snapshot_support=True revert_to_snapshot_support=True mount_snapshot_support=True'"
 export DEVSTACK_LOCAL_CONFIG+=$'\n'"SHARE_DRIVER=manila.share.drivers.lvm.LVMShareDriver"
 export DEVSTACK_LOCAL_CONFIG+=$'\n'"MANILA_OPTGROUP_generic1_driver_handles_share_servers=False"
+export FUXI_VOLUME_PROVIDERS=cinder,manila
 
 $BASE/new/devstack-gate/devstack-vm-gate.sh
