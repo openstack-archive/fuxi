9250eceb7dfc674d8425eac591ab1dfe46eff2ed
Hongbin Lu <hongbin.lu@huawei.com>
2017-04-29 12:03:54 -0400

    Move to devstack-plugin-container
    Change-Id: I39f7dec7e0b2ba93d0a0e9666c98cf9eb09bcfe1

--- fuxi/tests/contrib/gate_hook.sh
+++ fuxi/tests/contrib/gate_hook.sh
@@ -7,6 +7,7 @@ VENV=${1:-"fullstack"}
 GATE_DEST=$BASE/new
 DEVSTACK_PATH=$GATE_DEST/devstack
 
+export DEVSTACK_LOCAL_CONFIG+=$'\n'"enable_plugin devstack-plugin-container https://git.openstack.org/openstack/devstack-plugin-container"
 export DEVSTACK_LOCAL_CONFIG+=$'\n'"enable_plugin manila git://git.openstack.org/openstack/manila"
 export DEVSTACK_LOCAL_CONFIG+=$'\n'"MANILA_DEFAULT_SHARE_TYPE_EXTRA_SPECS='snapshot_support=True create_share_from_snapshot_support=True revert_to_snapshot_support=True mount_snapshot_support=True'"
 export DEVSTACK_LOCAL_CONFIG+=$'\n'"SHARE_DRIVER=manila.share.drivers.lvm.LVMShareDriver"
